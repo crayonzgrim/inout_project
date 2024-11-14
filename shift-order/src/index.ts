88305dce1cbef1d649fa0b951df24876794ad1d3
KIM DONG IL <dongil.kim@anti-agingclub.kr>
2024-11-14 09:30:42 +0900

    :hammer: Shift-order 질문 옮기기

--- shift-order/src/index.ts
+++ shift-order/src/index.ts
@@ -1,1 +1,27 @@
-console.log("SHIFT_ORDER");
+/**
+ * selected 배열에 포함된 data 배열의 원소들을 왼쪽으로 한 칸씩 옴기는 콘솔 프로그램을 작성해주세요.
+ *
+ * 예) data = [1, 2, 3], selected = [1],       data' = [1, 2, 3]
+ * 예) data = [1, 2, 3], selected = [2],       data' = [2, 1, 3]
+ * 예) data = [1, 2, 3], selected = [3],       data' = [1, 3, 2]
+ * 예) data = [1, 2, 3], selected = [1, 2],    data' = [1, 2, 3]
+ * 예) data = [1, 2, 3], selected = [2, 1],    data' = [1, 2, 3]
+ * 예) data = [1, 2, 3], selected = [1, 3],    data' = [1, 3, 2]
+ * 예) data = [1, 2, 3], selected = [3, 1],    data' = [1, 3, 2]
+ * 예) data = [1, 2, 3], selected = [2, 3],    data' = [2, 3, 1]
+ * 예) data = [1, 2, 3], selected = [3, 2],    data' = [2, 3, 1]
+ * 예) data = [1, 2, 3], selected = [1, 2, 3], data' = [1, 2, 3]
+ *
+ * 입력값에 오류는 없다고 가정합니다.
+ *
+ * selected는 data의 부분집합 입니다.
+ *
+ * 언어는 자바스크립트나 타입스크립트 모두 무방합니다.
+ */
+
+const data = [1, 2, 3];
+const selected = [1];
+
+// 아래에 코드를 작성해주세요.
+
+console.log(data);
