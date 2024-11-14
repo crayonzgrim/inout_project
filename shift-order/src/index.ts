c3d7f905394f28a6ea545500bd3973392fb9793d
KIM DONG IL <dongil.kim@anti-agingclub.kr>
2024-11-15 08:53:52 +0900

    :hammer: Shift order calculator

--- shift-order/src/index.ts
+++ shift-order/src/index.ts
@@ -19,9 +19,35 @@
  * 언어는 자바스크립트나 타입스크립트 모두 무방합니다.
  */
 
-const data = [1, 2, 3];
-const selected = [1];
+// const data = [1, 2, 3];
+// const selected = [1];
 
 // 아래에 코드를 작성해주세요.
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+console.clear();
 
-console.log(data);
+// 상수값
+const data = [1, 2, 3] as const;
+// 입력값
+const selected = [2, 3];
+const sortedSelect = selected.sort((a, b) => a - b);
+
+function shiftLeft(data: readonly number[], select: number[]): number[] {
+  const result = [...data];
+
+  for (let i = 0; i < select.length; i++) {
+    const index = result.indexOf(select[i]);
+
+    if (index > 0 && !select.includes(result[index - 1])) {
+      [result[index - 1], result[index]] = [result[index], result[index - 1]];
+    }
+  }
+
+  return result;
+}
+
+const result = shiftLeft(data, sortedSelect);
+
+// 결과 출력
+console.log(result);
